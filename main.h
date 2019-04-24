f00a611150bf8bb62d5c27e5bf965abfca723dd7
Morgan Nolan <morghayn@users.noreply.github.com>
2019-04-24 05:44:58 +0100

    Unit Creation: allocate_fare.cpp
    Changelog//
    + Addition of the allocate_fare class
    + Implementations into main.cpp (the main menu)
    + Addition of 'status' row to both vehicles and drivers, this will be used by the operator to determine whether they are active or inactive.
    
    * Renamed buttons.tsv to config.tsv
    config.tsv is going to be where all the default string constants are going to be stored. This includes such string constants as button names, default fare field names and so on. It may even eventually contain some console config settings that we might be changing later under the 'Config' primary button at the main menu.
    
    * All C++ header files are now located in merely one header file, main.h.
    My experience as far as header files goes says this is an efficient idea, however I do have belief this may lead to issues.
    
    * The Draw_Box function has been reallocated to Utilities.CPP as there's reusability of this function in both allocate_fare and main_menu class

--- main.h
+++ main.h
@@ -1,44 +1,87 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <conio.h>
 #include <numeric>
 
-#include "utilities.h"
-#include "utilities.cpp"
+#define _WIN32_WINNT 0x0500
+#include <windows.h>
 
 using namespace std;
+
+class Utilities {
+    public:
+        void Set_Console();
+        void gotoXY(int x, int y);
+        void gotoXY(int x, int y, char text);
+        void gotoXY(int x, int y, string text);
+        void gotoXY(int x, int y, string text, bool highlight);
+        void ShowConsoleCursor(bool showFlag);
+        vector< vector<string> > Generate_Two_Dimensional_Vector(string file_name);
+        void Draw_Box(string border_title, int x_position, int y_position, bool highlight_box);
+    protected:
+        string double_line, single_line;
+        int console_column_quantity, console_row_quantity, column_spacing, table_start, box_width, box_height;
+};
 
 class MainMenu: public Utilities {
     public:
         MainMenu();
         void Add_Menu_Title();
         void Selection_Value(bool key_down);
-        void Execute();
         void Print();
-
+        void Execute();
         void Update_Button_Description();
-        void Update_Buttons();  void Draw_Button(int x_position, int y_position, bool selection_status);
-        void Update_Boxes();    void Draw_Box(string border_title, int x_position, int y_position, bool selection_status);
+        void Update_Boxes();
+        void Update_Buttons();
+        void Draw_Button(int x_position, int y_position, bool selection_status);
+
     private:
         bool exit_trigger, primary_selected;
         vector< vector<string> > button_name, button_description;
-        int ran_1, ran_2, box_x_position[3], selection[3];
+        int primary, secondary, box_x_position[3], selection[3];
 };
 
-
 class Print_TSV_Table: public Utilities {
-    private:
-        int selected_page; vector< vector<string> > table; vector<int> optimal_cell_position; vector<int> page_index;
     public:
-        Print_TSV_Table(); void Print_Table(string file_name);
+        Print_TSV_Table(string file_name, string table_title);
         void Generate_Table_Vector(string file_name);
         void Determine_Optimal_Column_Positions(); int * Determine_Longest_Cells_In_Columns();
-        void Determine_Page_Indexing();
+        void Create_Table_Pages();
         void Draw_Table_Descriptors(string table_title, int page_quantity);
         void Draw_Table_Grid();
         void Print_Cells();
+
+    private:
+        int selected_page;
+        vector< vector<string> > table;
+        vector<int> page_range, optimal_cell_position;
 };
 
+class Allocate_Fare: public Utilities {
+    public:
+        Allocate_Fare();
+        void Add_Menu_Title();
+        void Selection_Value(int key_value);
+        void Print();
+        void Execute();
+        void Update_Boxes();
+        void Update_Fields();
+        void Text_Input_Functionality(int selected);
+        void Draw_Field(int x_position, int y_position, bool selection_status);
+        void Draw_Button(int x_position, int y_position, bool selection_status);
+
+    private:
+        bool text_input_mode;
+
+
+        bool exit_trigger, primary_selected;
+        vector< vector<string> > field_name;
+        int field_value, box_x_position[2], selection;
+};
 
 #endif // MAIN_H
--- utilities.cpp
+++ utilities.cpp
@@ -1,41 +1,31 @@
-#include "utilities.h"
+#include "main.h"
 
 HANDLE myConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD CursorPosition;
-
-
-void Utilities::gotoXY(int x, int y) {
-	CursorPosition.X = x; CursorPosition.Y = y;
-	SetConsoleCursorPosition(myConsole,CursorPosition);
-}
-
-
-void Utilities::gotoXY(int x, int y, char text) {
-	CursorPosition.X = x; CursorPosition.Y = y;
-	SetConsoleCursorPosition(myConsole,CursorPosition);
-	cout << text;
-}
-
-
-void Utilities::gotoXY(int x, int y, string text, bool highlight) {
-	CursorPosition.X = x; CursorPosition.Y = y;
-	SetConsoleCursorPosition(myConsole,CursorPosition);
-
-    int myColour = 15;
-    if(highlight == 1) {myColour = 240;}
-    SetConsoleTextAttribute(myConsole, myColour);
-	cout << text;
-    SetConsoleTextAttribute(myConsole, 15);
-}
+COORD cursor_position;
 
 
 void Utilities::ShowConsoleCursor(bool showFlag) {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO     cursorInfo;
+    CONSOLE_CURSOR_INFO cursorInfo;
 
     GetConsoleCursorInfo(out, &cursorInfo);
     cursorInfo.bVisible = showFlag; // set the cursor visibility
     SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+
+void Utilities::Set_Console() {
+    console_column_quantity = 140, console_row_quantity = 30; stringstream ss;
+    ss << "MODE CON: COLS=" << console_column_quantity << "LINES=" << console_row_quantity;
+    system(ss.str().c_str());
+
+    double_line = string (console_column_quantity,'\xCD');
+    single_line = string (console_column_quantity,'\xC4');
+
+    HWND consoleWindow = GetConsoleWindow();
+    SetWindowLong(consoleWindow, GWL_STYLE, GetWindowLong(consoleWindow, GWL_STYLE) & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);
+    ShowConsoleCursor(0);
+    table_start = 3;
 }
 
 
@@ -52,13 +42,45 @@ vector< vector<string> > Utilities::Generate_Two_Dimensional_Vector(string file_
 }
 
 
-void Utilities::Set_Console() {
-    console_column_quantity = 140, console_row_quantity = 30, vertical_outset = 3;
-    stringstream ss; ss << "MODE CON: COLS=" << console_column_quantity << "LINES=" << console_row_quantity;
-    string console_size = ss.str(); system(console_size.c_str());
-    double_line = string (console_column_quantity,'\xCD');
-    single_line = string (console_column_quantity,'\xC4');
+void Utilities::Draw_Box(string border_title, int x_position, int y_position, bool highlight_box) {
+    gotoXY(x_position + ((box_width - border_title.length()) / 2), y_position - 1);
+    cout << border_title; int box_colour = 8;
 
-    ShowConsoleCursor(0); HWND consoleWindow = GetConsoleWindow();
-    SetWindowLong(consoleWindow, GWL_STYLE, GetWindowLong(consoleWindow, GWL_STYLE) & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);
+    if (highlight_box == true) box_colour = 3; SetConsoleTextAttribute(myConsole, box_colour);
+    gotoXY(x_position, y_position);
+    cout << string(box_width, '\xDC');
+    gotoXY(x_position, (y_position + box_height - 1));
+    cout << string(box_width, '\xDF');
+
+    for (int i = 0; i < (box_height - 2); i++) {
+        gotoXY(x_position, i + (y_position + 1));
+        cout << char('\xBA');
+        gotoXY(x_position + (box_width - 1), i + (y_position + 1));
+        cout << char('\xBA');
+    }   SetConsoleTextAttribute(myConsole, 15);
+}
+
+
+void Utilities::gotoXY(int x, int y) {
+	cursor_position.X = x; cursor_position.Y = y;
+	SetConsoleCursorPosition(myConsole,cursor_position);
+}
+
+void Utilities::gotoXY(int x, int y, char text) {
+	cursor_position.X = x; cursor_position.Y = y;
+	SetConsoleCursorPosition(myConsole,cursor_position); cout << text;
+}
+
+void Utilities::gotoXY(int x, int y, string text) {
+	cursor_position.X = x; cursor_position.Y = y;
+	SetConsoleCursorPosition(myConsole,cursor_position); cout << text;
+}
+
+void Utilities::gotoXY(int x, int y, string text, bool highlight) {
+	cursor_position.X = x; cursor_position.Y = y;
+	SetConsoleCursorPosition(myConsole,cursor_position);
+
+    int myColour = 15; if(highlight == 1) myColour = 240;
+    SetConsoleTextAttribute(myConsole, myColour); cout << text;
+    SetConsoleTextAttribute(myConsole, 15);
 }
